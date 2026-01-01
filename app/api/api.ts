3d64dae7a1c751db33bd84be139ebed82a5ffa58
Marina <marinamaksimovskaa3@gmail.com>
2026-01-01 17:20:10 +0200

    ffix

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,6 +1,7 @@
+// app/api/api.ts
 import axios from "axios";
 
-export const nextServer = axios.create({
+export const api = axios.create({ 
   baseURL:
     (process.env.NEXT_PUBLIC_API_URL || "https://09-auth-six-navy.vercel.app") +
     "/api",
