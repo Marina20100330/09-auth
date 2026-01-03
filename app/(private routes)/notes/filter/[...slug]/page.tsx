390c6f8828295bec0eadebff2a55048aaf1868eb
Marina <marinamaksimovskaa3@gmail.com>
2026-01-03 19:49:23 +0200

    fix

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -1,34 +1,35 @@
-import NotesClient from "../[...slug]/Notes.client";
-import { fetchNotes } from "@/lib/api/serverApi";
+import NotesClient from "./Notes.client"; 
+import { fetchNotes } from "@/lib/api/clientApi"; 
 import type { Metadata } from "next";
 import type { CategoryNoAll } from "@/types/note";
-
 import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
 
 export default async function Page({ params }: { params: { slug: string[] } }) {
- 
   const tagNote = params.slug[0] === "all" ? undefined : (params.slug[0] as CategoryNoAll);
 
   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
-    queryKey: ["notes", "", 1, tagNote], 
-    queryFn: () => fetchNotes("", 1, tagNote),
-  });
+  try {
+    await queryClient.prefetchQuery({
+     
+      queryKey: ["notes", "", 1, tagNote], 
+      queryFn: () => fetchNotes("", 1, tagNote), 
+    });
+  } catch (error) {
+    console.error("Prefetch error:", error);
+  }
 
   return (
-      <HydrationBoundary state={dehydrate(queryClient)}>
-      {}
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <NotesClient initialTag={tagNote} />
     </HydrationBoundary>
   );
 }
 
-
 export async function generateMetadata({ params }: { params: { slug: string[] } }): Promise<Metadata> {
   const category = params.slug[0];
   return {
+   
     title: `Notes: ${category} | NoteHub`,
   };
 }
-
