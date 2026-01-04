db395bbf0647ef8d8b85c936c7fa935c0ce1bfd6
Marina <marinamaksimovskaa3@gmail.com>
2026-01-04 19:37:31 +0200

    nn

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -19,13 +19,19 @@ export async function generateMetadata({
   const { id } = await params;
   const note = await fetchNoteById(id);
 
+  if (!note) {
+    return {
+      title: "Note Not Found | NoteHub",
+    };
+  }
+
   return {
     title: `Note: ${note.title}`,
     description: note.content.slice(0, 30),
     openGraph: {
       title: `Note: ${note.title}`,
       description: note.content.slice(0, 100),
-      url: `https://08-zustand-puce-seven.vercel.app/notes/${id}`,
+      url: `https://09-auth-five-fawn.vercel.app/notes/${id}`,
       siteName: "NoteHub",
       type: "article",
       images: [
