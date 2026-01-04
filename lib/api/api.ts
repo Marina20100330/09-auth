cb23fc0c2b7eec1bcfb883830a387f9b2e43574f
Marina <marinamaksimovskaa3@gmail.com>
2026-01-04 15:33:01 +0200

    ff

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const isServer = typeof window === "undefined";
+
 export const nextServer = axios.create({
-  baseURL:
-    (process.env.NEXT_PUBLIC_API_URL || "https://09-auth-six-navy.vercel.app") +
-    "api", 
+  baseURL: isServer
+    ? "https://notehub-api.goit.study"
+    : (process.env.NEXT_PUBLIC_API_URL || "https://09-auth-brown-eight.vercel.app") + "/api",
   withCredentials: true,
 });
+
+export default nextServer;
